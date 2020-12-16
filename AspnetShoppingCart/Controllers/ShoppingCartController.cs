5e8739734b89bb4240688ee5b30b853adf110f64
Billy Huang <10646003@ntub.edu.tw>
2020-12-16 22:46:28 +0800

    feat(shopping-cart): add add action

--- AspnetShoppingCart/Controllers/ShoppingCartController.cs
+++ AspnetShoppingCart/Controllers/ShoppingCartController.cs
@@ -27,5 +27,43 @@ namespace AspnetShoppingCart.Controllers
             // 指定 Index.cshtml 套用 _LayoutMember.cshtml，View 使用 orderDetails
             return View("Index", "_LayoutMember", orderDetails);
         }
+
+        // GET: ShoppingCart/Add
+        public ActionResult Add(string pId)
+        {
+            // 取得登入會員帳號並指定給 userId
+            string userId = (Session["Member"] as Member).UserId;
+
+            // 找出會員放入訂單明細的產品
+            // 該產品的 IsApproved 為 "否"，表示該產品是購物車狀態
+            var currentCart = db.OrderDetail
+                .Where(m => m.PId == pId && m.IsApproved == "否" && m.UserId == userId)
+                .FirstOrDefault();
+
+            // 若 currentCart 等於 null，表示會員選購的產品不是購物車狀態
+            if (currentCart == null)
+            {
+                // 找出目前選購的產品並指定給 product
+                var product = db.Product.Where(m => m.PId == pId).FirstOrDefault();
+                // 將產品放入訂單明細，因為產品的 IsApproved 為 "否"，表示為購物車狀態
+                OrderDetail orderDetail = new OrderDetail();
+                orderDetail.PId = product.PId;
+                orderDetail.Name = product.Name;
+                orderDetail.Price = product.Price;
+                orderDetail.Quantity = 1;
+                orderDetail.IsApproved = "否";
+                orderDetail.UserId = userId;
+                db.OrderDetail.Add(orderDetail);
+            }
+            else
+            {
+                // 若產品為購物車狀態，即將該產品數量加 1
+                currentCart.Quantity += 1;
+            }
+            db.SaveChanges();
+
+            // 執行 SoppingCart 控制器的 Index 動作方法
+            return RedirectToAction("Index");
+        }
     }
 }
